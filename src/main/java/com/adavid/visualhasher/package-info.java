c1d03ba53c3b1923a7aaa0283a31b422a27e02a4
d19006523 <axel.david@etu.univ-amu.fr>
2022-12-31 15:00:08 +0100

    Update package-info.java

--- src/main/java/com/adavid/visualhasher/package-info.java
+++ src/main/java/com/adavid/visualhasher/package-info.java
@@ -15,55 +15,24 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-/*
- * VisualHasher Copyright (C) 2022 DAVID Axel
+/**
+ * The VisualHasher application package.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * <br><br>
+ * <u><b>Packages list:</b></u><br>
+ * - {@link com.adavid.visualhasher.application}<br>
+ * - {@link com.adavid.visualhasher.domain}<br>
+ * - {@link com.adavid.visualhasher.infrastructure}<br>
+ * - {@link com.adavid.visualhasher.presentation}<br>
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * <br><br>
+ * <u><b>Classes list:</b></u><br>
+ * - {@link com.adavid.visualhasher.Application}<br>
+ * - {@link com.adavid.visualhasher.SwingApplication}<br>
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * @version 1.0.0
+ * @author Axel DAVID
+ * @see com.adavid.visualhasher.Application
+ * @since 1.0.0
  */
-
-/*
- * VisualHasher Copyright (C) 2022 DAVID Axel
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-/*
- * VisualHasher Copyright (C) 2022 DAVID Axel
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 package com.adavid.visualhasher;
